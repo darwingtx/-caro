a58a4d3d03971c2607ed0363ee18724de69d1bed
Darwin Castaño <dc162005@gmail.com>
2023-12-04 19:12:03 -0500

    Modo oscuro y modo claro

--- Assets/JS/CerrarSesion.js
+++ Assets/JS/CerrarSesion.js
@@ -1,18 +1,15 @@
 $(document).ready(function () {
-    // Obtener el botón de cerrar sesión
+
     var cerrarSesionBtn = $('#Sesion');
 
-    // Manejar el evento de clic en el botón de cerrar sesión
     cerrarSesionBtn.on('click', function () {
         cerrarSesion();
     });
 
-    // Función para cerrar sesión
     function cerrarSesion() {
-        // Eliminar la información de la sesión actual en localStorage
         localStorage.removeItem('usuarioActual');
-
-        // Redirigir al usuario a la página de inicio de sesión
         window.location.href = './Inicio.html';
     }
+
+
 });
